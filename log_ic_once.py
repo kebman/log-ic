6613dc87b82bd9864f1bdb7429ef358641f4da5c
kebman <kebman@journalist.com>
2021-06-30 07:15:24 +0200

    Update log_ic_once.py

--- log_ic_once.py
+++ log_ic_once.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # log internet access once - using sqlite
+from datetime import datetime
 import time 
 import sqlite3
 import socket
 import random
 
 # database connection
-con = sqlite3.connect('./db/ic_log1_2020-06-22_manual.db')
+con = sqlite3.connect('./db/ic_log1_2020-06-30_manual.db')
 cur = con.cursor()
 
 # db table: iclog (date real, ic integer, error text)
@@ -32,30 +33,31 @@ def check_treshold(ts, treshold_time):
 # 1. check if there's an outage (1 yes, there's an outage vs. 0 no, there's no outage)
 
 # Choose a random server, to lessen the load on any single server, and thus lessen the chance of being banned
-servers = ['1.1.1.1','4.2.2.1','4.2.2.2','4.2.2.3','4.2.2.4','4.2.2.5','4.2.2.6','8.8.4.4','8.8.8.8','208.67.220.220','208.67.222.222']
+servers = ['1.1.1.1','4.2.2.1','4.2.2.2','4.2.2.3','4.2.2.4','4.2.2.5','4.2.2.6','8.8.4.4','8.8.8.8','208.67.220.220','208.67.222.222', '80.202.2.2', '217.13.0.2']
 random_pick = random.choice(servers)
 print("Testing server", random_pick)
 
 def is_connected():
 	ic = dict()
 
-	s = socket.socket()
-	s.settimeout(0.03)
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.settimeout(1)
 
 	try:
-		# cnx to dns server on a random port above 1023 for tcp
-		s.connect((random_pick, 33061))
+		host = socket.gethostbyaddr(random_pick)[0]
 
 		ic['ts'] = time.time()
 		ic['outage'] = 0
-		return ic
+		ic['note'] = host
 
+		return ic
 	except OSError as err:
+	# except socket.error as err:
 		ic['ts'] = time.time()
 		ic['outage'] = 1
-		ic['err'] = err
+		ic['note'] = err
 		return ic
-	
+
 	finally:
 		s.close()
 
@@ -68,18 +70,11 @@ test = is_connected()
 cur.execute('SELECT * FROM iclog ORDER BY date DESC LIMIT 1')
 db_result = cur.fetchone()
 
-# make timestame human readable
-def ts_to_txt(ts):
-	return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-# make dasta human readable and print it out
-print("Last log entry:", ts_to_txt(db_result[0]), "UTC.", "Online." if db_result[1] == 0 else "Offline.", "Error: %s" %(db_result[2]) if db_result[2].isalpha() else "")
-
 # 4. tests on last entry:
 
 # 4*. if answer is 0 (no), and last entry is None, then make the first entry in the database :D
 if test['outage'] == 0 and db_result == None:
-	cur.execute("INSERT INTO iclog VALUES (?, ?, ?)", (test['ts'], test['outage'], "",))
+	cur.execute("INSERT INTO iclog VALUES (?, ?, ?)", (test['ts'], test['outage'], test['note'],))
 	con.commit()
 	con.close()
 	print("You're connected to the Internet.")
@@ -93,7 +88,7 @@ else:
 
 # 4b. if answer is 0 (no), and last entry is 1 (yes), then make a new entry in the database
 if test['outage'] == 0 and db_result[1] == 1:
-	cur.execute("INSERT INTO iclog VALUES (?, ?, ?)", (test['ts'], test['outage'], "",))
+	cur.execute("INSERT INTO iclog VALUES (?, ?, ?)", (test['ts'], test['outage'], test['note'],))
 	con.commit()
 	con.close()
 	print("Yippee! You're connected to the Internet again!")
@@ -109,7 +104,7 @@ if test['outage'] == 1 and db_result[1] == 1:
 
 	# 4ci. if last entry timestamp is more than TRESHOLD time ago, then make a new entry in the database	
 	if result == True:
-		cur.execute("INSERT INTO iclog VALUES (?, ?, ?)", (test['ts'], test['outage'], "",))
+		cur.execute("INSERT INTO iclog VALUES (?, ?, ?)", (test['ts'], test['outage'], test['note'],))
 		con.commit()
 		con.close()
 		print("No Internet connection...")
@@ -122,3 +117,10 @@ if test['outage'] == 1 and db_result[1] == 1:
 else:
 	pass
 	# print("So no treshold calculated")
+
+# make timestame human readable
+def ts_to_txt(ts):
+	return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+# make dasta human readable and print it out
+print("Last log entry:", ts_to_txt(db_result[0]), "UTC.", "Online." if db_result[1] == 0 else "Offline.", "Note: %s" %(db_result[2]))
